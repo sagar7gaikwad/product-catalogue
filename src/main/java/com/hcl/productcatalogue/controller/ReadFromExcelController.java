9e3da02337c1ca3c45538727cf511e773d6c0a2e
sontrive <sontrive0310@gmail.com>
2019-07-31 17:41:14 +0530

    commit

--- src/main/java/com/hcl/productcatalogue/controller/ReadFromExcelController.java
+++ src/main/java/com/hcl/productcatalogue/controller/ReadFromExcelController.java
@@ -1,8 +1,25 @@
 package com.hcl.productcatalogue.controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hcl.productcatalogue.exception.ApplicationException;
+import com.hcl.productcatalogue.service.ReadFromExcelService;
 
 @RestController
 public class ReadFromExcelController {
+	
+	
+	@Autowired
+	ReadFromExcelService readFromExcelService;
 
+	
+	@GetMapping("")
+	public void readProducts() throws ApplicationException, IOException {
+		readFromExcelService.readProducts();
+		 
+	}		
 }
