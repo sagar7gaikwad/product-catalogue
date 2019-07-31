693c5d4d44936a431457099a0cab1c9163b188a9
sagar7gaikwad <sagar7gaikwad@gmail.com>
2019-07-31 16:55:20 +0530

    change

--- src/main/java/com/hcl/productcatalogue/dto/ProductDTO.java
+++ src/main/java/com/hcl/productcatalogue/dto/ProductDTO.java
@@ -1,8 +1,10 @@
 package com.hcl.productcatalogue.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ProductDTO {
 	
 			private String name;
--- src/main/java/com/hcl/productcatalogue/serviceimpl/GetProductServiceImpl.java
+++ src/main/java/com/hcl/productcatalogue/serviceimpl/GetProductServiceImpl.java
@@ -1,11 +1,13 @@
 package com.hcl.productcatalogue.serviceimpl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import com.hcl.productcatalogue.dto.ProductDTO;
 import com.hcl.productcatalogue.dto.ResponseDTO;
 import com.hcl.productcatalogue.entity.Product;
 import com.hcl.productcatalogue.repository.ProductRepository;
@@ -20,12 +22,14 @@ public class GetProductServiceImpl implements GetProductService {
 	public ResponseDTO getAllProducts() {
 		
 		List<Product> productList = productRepository.getAllOrderByProductVersionDsc();
-		System.out.println(productList);
+		
+		List<ProductDTO> productResponseDTOList = productList.stream().map(i -> new ProductDTO(i.getName(),i.getCategory()
+				,i.getPrice(), i.getQuantity())).collect(Collectors.toList());
 		
 		ResponseDTO responseList = new ResponseDTO();
 		responseList.setHttpStatus(HttpStatus.OK);
 		responseList.setMessage("List Of Latest Product Catalogue");
-		responseList.setData(productList);
+		responseList.setData(productResponseDTOList);
 		
 		return responseList;
 	}
