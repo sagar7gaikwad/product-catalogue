00268001e5d74e6a85ffc4af88e8e91445cd8790
pracheerjain <pracheer2002@gmail.com>
2019-07-31 18:17:47 +0530

    update db coding and junit

--- src/main/java/com/hcl/productcatalogue/serviceimpl/ReadFromExcelServiceImpl.java
+++ src/main/java/com/hcl/productcatalogue/serviceimpl/ReadFromExcelServiceImpl.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -25,20 +22,17 @@ public class ReadFromExcelServiceImpl implements ReadFromExcelService {
 	@Autowired
 	ProductRepository productRepository;
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings({ "rawtypes" })
 	public List<Product> readProducts() {
 		// An excel file name. You can create a file name with a full path information.
 
 		String filename = "C:\\Users\\Administrator\\Desktop\\products.xlsx";
 
-		// Create an ArrayList to store the data read from excel sheet.
-		List sheetData = new ArrayList();
 		FileInputStream fis = null;
 		List<Product> proList = new ArrayList<Product>();
-		
+
 		try {
 
-			
 			// Create a FileInputStream that will be use to read the excel file.
 			fis = new FileInputStream(filename);
 
@@ -53,18 +47,18 @@ public class ReadFromExcelServiceImpl implements ReadFromExcelService {
 			// each sheet's rows and on each row's cells. We store the
 			// data read on an ArrayList so that we can printed the
 			// content of the excel to the console.
-			
+
 			productRepository.deleteAll();
-			
+
 			Iterator rows = sheet.rowIterator();
 			while (rows.hasNext()) {
 				XSSFRow row = (XSSFRow) rows.next();
 				Product prod = new Product();
 				prod.setName(row.getCell(0).toString());
-				prod.setQuantity((int)Double.parseDouble(row.getCell(3).toString()));
+				prod.setQuantity((int) Double.parseDouble(row.getCell(3).toString()));
 				prod.setPrice(Double.parseDouble(row.getCell(2).toString()));
-				 prod.setCategory(row.getCell(1).toString());
-				
+				prod.setCategory(row.getCell(1).toString());
+
 				productRepository.save(prod);
 				proList.add(prod);
 
@@ -81,30 +75,7 @@ public class ReadFromExcelServiceImpl implements ReadFromExcelService {
 			}
 		}
 
-return proList;
+		return proList;
 	}
 
-	private static void showExcelData(List sheetData) {
-		//
-		// Iterates the data and print it out to the console.
-		//
-		for (int i = 0; i < sheetData.size(); i++) {
-			@SuppressWarnings("rawtypes")
-			List list = (List) sheetData.get(i);
-			
-			
-			for (int j = 0; j < list.size(); j++) {
-				Cell cell = (Cell) list.get(j);
-				if (cell.getCellType() == CellType.NUMERIC) {
-					System.out.print(cell.getNumericCellValue());
-				} else if (cell.getCellType() == CellType.STRING) {
-					System.out.print(cell.getRichStringCellValue());
-				}
-				if (j < list.size() - 1) {
-					System.out.print(", ");
-				}
-			}
-			System.out.println("");
-		}
-	}
 }
